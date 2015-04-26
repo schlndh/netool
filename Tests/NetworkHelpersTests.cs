d68afd3f8faa3974cadb8acb0f55987a6b1af9ad
Hynek Schlindenbuch <schlindenbuch.h@seznam.cz>
2015-04-26 14:13:10 +0200

    IPEndPoint Parse and TryParse methods

---
+++ Tests/NetworkHelpersTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using Netool.Network.Helpers;
+namespace Tests
+{
+    [TestClass]
+    public class IPEndPointParserTest
+    {
+        [TestMethod]
+        public void TestValid()
+        {
+            IPEndPoint ep;
+            ep = new IPEndPoint(IPAddress.Loopback, 0);
+            Assert.AreEqual(ep, IPEndPointParser.Parse(ep.ToString()));
+            ep = new IPEndPoint(IPAddress.IPv6Loopback, 1234);
+            Assert.AreEqual(ep, IPEndPointParser.Parse(ep.ToString()));
+            ep = new IPEndPoint(IPAddress.Parse("10.11.12.13"), 1234);
+            Assert.AreEqual(ep, IPEndPointParser.Parse(ep.ToString()));
+            ep = new IPEndPoint(IPAddress.Parse("2001:db8:85a3:0:0:8a2e:370:7334"), 0);
+            Assert.AreEqual(ep, IPEndPointParser.Parse(ep.ToString()));
+            ep = new IPEndPoint(IPAddress.Parse("2001::7334"), 1234);
+            Assert.AreEqual(ep, IPEndPointParser.Parse(ep.ToString()));
+        }
+        [TestMethod]
+        public void TestInvalid()
+        {
+            IPEndPoint ep;
+            Assert.IsFalse(IPEndPointParser.TryParse(null, out ep));
+            Assert.IsFalse(IPEndPointParser.TryParse("", out ep));
+            Assert.IsFalse(IPEndPointParser.TryParse("132", out ep));
+            Assert.IsFalse(IPEndPointParser.TryParse("300.0.0.1:1000", out ep));
+            Assert.IsFalse(IPEndPointParser.TryParse("gggg::1", out ep));
+            Assert.IsFalse(IPEndPointParser.TryParse("127.0.0.1:-1", out ep));
+            Assert.IsFalse(IPEndPointParser.TryParse("::1:70000", out ep));
+        }
+    }
+}
---
+++ netool/Network/Helpers.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+namespace Netool.Network.Helpers
+{
+    public static class IPEndPointParser
+    {
+        public static IPEndPoint Parse(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str)) throw new FormatException("Invalid IPEndpoint string - empty string!");
+            var i = str.LastIndexOf(':');
+            if(i >= 0)
+            {
+                // IPEndPoint.ToString() wraps IPv6 addresses in []
+                var ipstr = str.Substring(0, i).Replace("[","").Replace("]","");
+                str.Substring(i + 1);
+                int port;
+                if(int.TryParse(str.Substring(i+1), out port) && port >= 0 && port < 65536)
+                {
+                    return new IPEndPoint(IPAddress.Parse(ipstr), port);
+                }
+                else
+                {
+                    throw new FormatException("Invalid IPEndPoint string - invalid port number!");
+                }
+            }
+            else
+            {
+                throw new FormatException("Invalid IPEndPoint string - no port number!");
+            }
+        }
+        public static bool TryParse(string str, out IPEndPoint ep)
+        {
+            try
+            {
+                ep = Parse(str);
+                return true;
+            }
+            catch(FormatException)
+            {
+                ep = null;
+                return false;
+            }
+        }
+    }
+}
