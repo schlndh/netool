fc94abf5d30071508af534ce67341a25ee508217
Hynek Schlindenbuch <schlindenbuch.h@seznam.cz>
2015-06-26 20:28:17 +0200

    unlimited option for ManualChannelDriver

--- netool/ChannelDrivers/ManualChannelDriver.cs
+++ netool/ChannelDrivers/ManualChannelDriver.cs
@@ -7,10 +7,14 @@ namespace Netool.ChannelDrivers
     {
         private int capacity;
         private int activeChannels = 0;
-        public bool CanAccept(IChannel c) { return capacity > activeChannels; }
+        public bool CanAccept(IChannel c) { return capacity == -1 || capacity > activeChannels; }
         public bool AllowManualControl { get { return true; } }
         public string ID { get { return "Manual"; } }
 
+        /// <summary>
+        /// Creates Manual channel driver with specified capacity of active connections
+        /// </summary>
+        /// <param name="capacity">set -1 for unlimited capacity</param>
         public ManualChannelDriver(int capacity)
         {
             this.capacity = capacity;
