5ccc4db43248ebae65e4fad8f0714aacb9be08fc
Hynek Schlindenbuch <schlindenbuch.h@seznam.cz>
2015-08-29 20:36:44 +0200

    Basic scriptable channel driver using CS-Script

---
+++ libnetool/ChannelDrivers/CSScriptChannelDriver.cs
@@ -0,0 +1,53 @@
+﻿using CSScriptLibrary;
+using Netool.Network;
+using System;
+
+namespace Netool.ChannelDrivers
+{
+    [Serializable]
+    public class CSScriptChannelDriver : IChannelDriver
+    {
+        /// <inheritdoc/>
+        public bool AllowManualControl { get { return innerDriver == null || innerDriver.AllowManualControl; } }
+
+        /// <inheritdoc/>
+        public string ID { get { return "CS-Script"; } }
+
+        /// <inheritdoc/>
+        public object Settings { get { return scriptFilename; } }
+
+        [NonSerialized]
+        private IChannelDriver innerDriver;
+
+        private string scriptFilename;
+
+        public CSScriptChannelDriver(string filename)
+        {
+            try
+            {
+                innerDriver = (IChannelDriver)CSScriptLibrary.CSScript
+                    .LoadCodeFrom(filename)
+                    .CreateObject("*")
+                    .AlignToInterface<IChannelDriver>(); ;
+                scriptFilename = filename;
+            }
+            catch(Exception e)
+            {
+                throw new InvalidSettingsException("Unable to load class from given file!", e);
+            }
+
+        }
+
+        /// <inheritdoc/>
+        public bool CanAccept(IChannel c)
+        {
+            return innerDriver.CanAccept(c);
+        }
+
+        /// <inheritdoc/>
+        public void Handle(IChannel c)
+        {
+            innerDriver.Handle(c);
+        }
+    }
+}
--- libnetool/ChannelDrivers/ChannelDrivers.cs
+++ libnetool/ChannelDrivers/ChannelDrivers.cs
@@ -1,7 +1,31 @@
 ﻿using Netool.Network;
+using System;
 
 namespace Netool.ChannelDrivers
 {
+    [Serializable]
+    public class ChannelDriverException : Exception
+    {
+        public ChannelDriverException() { }
+        public ChannelDriverException(string message) : base(message) { }
+        public ChannelDriverException(string message, Exception inner) : base(message, inner) { }
+        protected ChannelDriverException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context)
+            : base(info, context) { }
+    }
+
+    [Serializable]
+    public class InvalidSettingsException : ChannelDriverException
+    {
+      public InvalidSettingsException() { }
+      public InvalidSettingsException( string message ) : base( message ) { }
+      public InvalidSettingsException( string message, Exception inner ) : base( message, inner ) { }
+      protected InvalidSettingsException(
+	    System.Runtime.Serialization.SerializationInfo info,
+	    System.Runtime.Serialization.StreamingContext context ) : base( info, context ) { }
+    }
+
     /// <summary>
     /// Channel driver interface, must be serializable
     /// </summary>
