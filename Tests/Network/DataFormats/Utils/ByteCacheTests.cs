c37b1a0b9de078a3fde94436b0b2494232fc8443
Hynek Schlindenbuch <schlindenbuch.h@seznam.cz>
2015-11-18 14:09:07 +0100

    add ByteCache class

---
+++ Tests/Network/DataFormats/Utils/ByteCacheTests.cs
@@ -0,0 +1,44 @@
+﻿using Netool.Network.DataFormats.Utils;
+using System;
+using Xunit;
+
+namespace Tests.Network.DataFormats.Utils
+{
+    public class ByteCacheTests
+    {
+        [Fact]
+        public void TestEmpty()
+        {
+            var cache = new ByteCache(256);
+            byte res;
+            Assert.False(cache.TryReadByte(0, out res));
+            Assert.False(cache.TryReadByte(1, out res));
+        }
+
+        [Fact]
+        public void TestFilled()
+        {
+            ByteCache.FillCache callback = delegate(byte[] buffer, out long cacheStart, out int cacheLength)
+            {
+                cacheStart = 5;
+                cacheLength = 4;
+                buffer[0] = 5;
+                buffer[1] = 6;
+                buffer[2] = 7;
+                buffer[3] = 8;
+            };
+            var cache = new ByteCache(256);
+            byte res;
+            cache.Cache(callback);
+            Assert.Equal(5, cache.Start);
+            Assert.Equal(4, cache.Length);
+            Assert.False(cache.TryReadByte(0, out res));
+            Assert.False(cache.TryReadByte(4, out res));
+            for(int i = 0; i < 4; ++i)
+            {
+                Assert.True(cache.TryReadByte(i + 5, out res));
+                Assert.Equal(i + 5, res);
+            }
+        }
+    }
+}
---
+++ libnetool/Network/DataFormats/Utils/ByteCache.cs
@@ -0,0 +1,67 @@
+﻿namespace Netool.Network.DataFormats.Utils
+{
+    /// <summary>
+    /// Helper class for easily caching logical byte arrays
+    /// </summary>
+    public class ByteCache
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <param name="cacheStart">underlaying data index coresponding to buffer index 0</param>
+        /// <param name="cacheLength">how many data were cached</param>
+        public delegate void FillCache(byte[] buffer, out long cacheStart, out int cacheLength);
+
+        public int MaxCacheSize { get; private set; }
+        private byte[] buffer;
+
+        private long start;
+        private int length;
+
+        /// <summary>
+        /// Underlaying data index corresponding to buffer index 0
+        /// </summary>
+        public long Start { get { return start; } }
+
+        /// <summary>
+        /// Number of valid entries in cache
+        /// </summary>
+        public int Length { get { return length; } }
+
+        public ByteCache(int maxCacheSize)
+        {
+            MaxCacheSize = maxCacheSize;
+        }
+
+        /// <summary>
+        /// Try to read byte from cache
+        /// </summary>
+        /// <param name="index">absolute index (will be recalculated relatively to cacheStart)</param>
+        /// <param name="res">result</param>
+        /// <returns>false = cache miss (res = 0), true = cache hit</returns>
+        public bool TryReadByte(long index, out byte res)
+        {
+            if (buffer == null || index >= Start + Length || index < Start)
+            {
+                res = 0;
+                return false;
+            }
+            else
+            {
+                res = buffer[index - Start];
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Cache new data
+        /// </summary>
+        /// <param name="callback">caching callback to call</param>
+        public void Cache(FillCache callback)
+        {
+            if (buffer == null) buffer = new byte[MaxCacheSize];
+            callback(buffer, out start, out length);
+        }
+    }
+}
