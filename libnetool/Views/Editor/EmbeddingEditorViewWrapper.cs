dd8f876285fbde0b46acdb895ae17cc707aa0d83
Hynek Schlindenbuch <schlindenbuch.h@seznam.cz>
2015-12-15 11:14:55 +0100

    add wrapper classes for embedding editor/event views
    To resolve the problem with infinite embedding
    in IEventView/IEditorView I created two classes
    that should be used as a lazy-evalutation wrappers
    around views that embed other views. This by itself
    is not enough and you should *ALWAYS* choose a safe
    default view when embedding other views (eg. HexView)
    or choose no default view and leave it to user.

---
+++ libnetool/Views/Editor/EmbeddingEditorViewWrapper.cs
@@ -0,0 +1,60 @@
+﻿namespace Netool.Views.Editor
+{
+    /// <summary>
+    /// Use this class as a wrapper for editor views that embed other editor views,
+    /// to prevent infinite embedding.
+    /// </summary>
+    public class EmbeddingEditorViewWrapper : IEditorView
+    {
+        public delegate IEditorView EditorViewFactory();
+
+        /// <inheritdoc/>
+        public string ID { get; private set; }
+
+        private IEditorView view;
+
+        private EditorViewFactory factory;
+
+        public EmbeddingEditorViewWrapper(EditorViewFactory factory, string id)
+        {
+            ID = id;
+            this.factory = factory;
+        }
+
+        public void Clear()
+        {
+            if (view == null)
+            {
+                view = factory();
+            }
+            view.Clear();
+        }
+
+        public Network.DataFormats.IDataStream GetValue()
+        {
+            if (view == null)
+            {
+                view = factory();
+            }
+            return view.GetValue();
+        }
+
+        public void SetValue(Network.DataFormats.IDataStream s)
+        {
+            if (view == null)
+            {
+                view = factory();
+            }
+            view.SetValue(s);
+        }
+
+        public System.Windows.Forms.Form GetForm()
+        {
+            if (view == null)
+            {
+                view = factory();
+            }
+            return view.GetForm();
+        }
+    }
+}
---
+++ libnetool/Views/Event/EmbeddingEventViewWrapper.cs
@@ -0,0 +1,44 @@
+﻿namespace Netool.Views.Event
+{
+    /// <summary>
+    /// Use this class as a wrapper for event views that embed other event views,
+    /// to prevent infinite embedding.
+    /// </summary>
+    public class EmbeddingEventViewWrapper : IEventView
+    {
+        public delegate IEventView EventViewFactory();
+
+        /// <inheritdoc/>
+        public string ID { get; private set; }
+
+        private IEventView view;
+
+        private EventViewFactory factory;
+
+        public EmbeddingEventViewWrapper(EventViewFactory factory, string id)
+        {
+            ID = id;
+            this.factory = factory;
+        }
+
+        /// <inheritdoc/>
+        public void Show(Network.DataFormats.IDataStream s)
+        {
+            if (view == null)
+            {
+                view = factory();
+            }
+            view.Show(s);
+        }
+
+        /// <inheritdoc/>
+        public System.Windows.Forms.Form GetForm()
+        {
+            if (view == null)
+            {
+                view = factory();
+            }
+            return view.GetForm();
+        }
+    }
+}
