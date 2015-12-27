80171122d6f9dbcd3aba33d2a94284d5b874b9c2
Hynek Schlindenbuch <schlindenbuch.h@seznam.cz>
2015-12-27 15:05:03 +0100

    add TextBoxDialog

---
+++ libnetool/Dialogs/TextBoxDialog.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Netool.Dialogs
+{
+    partial class TextBoxDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.cancelBtn = new System.Windows.Forms.Button();
+            this.okBtn = new System.Windows.Forms.Button();
+            this.valueTextBox = new System.Windows.Forms.TextBox();
+            this.dialogLabel = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.CausesValidation = false;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.valueTextBox, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.dialogLabel, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(284, 261);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.CausesValidation = false;
+            this.flowLayoutPanel1.Controls.Add(this.cancelBtn);
+            this.flowLayoutPanel1.Controls.Add(this.okBtn);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 224);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(278, 34);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.CausesValidation = false;
+            this.cancelBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelBtn.Location = new System.Drawing.Point(200, 3);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(75, 23);
+            this.cancelBtn.TabIndex = 1;
+            this.cancelBtn.Text = "Cancel";
+            this.cancelBtn.UseVisualStyleBackColor = true;
+            // 
+            // okBtn
+            // 
+            this.okBtn.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.okBtn.Location = new System.Drawing.Point(119, 3);
+            this.okBtn.Name = "okBtn";
+            this.okBtn.Size = new System.Drawing.Size(75, 23);
+            this.okBtn.TabIndex = 0;
+            this.okBtn.Text = "OK";
+            this.okBtn.UseVisualStyleBackColor = true;
+            this.okBtn.Click += new System.EventHandler(this.okBtn_Click);
+            // 
+            // valueTextBox
+            // 
+            this.valueTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.valueTextBox.Location = new System.Drawing.Point(3, 184);
+            this.valueTextBox.Name = "valueTextBox";
+            this.valueTextBox.Size = new System.Drawing.Size(278, 20);
+            this.valueTextBox.TabIndex = 2;
+            this.valueTextBox.Validating += new System.ComponentModel.CancelEventHandler(this.valueTextBox_Validating);
+            // 
+            // dialogLabel
+            // 
+            this.dialogLabel.AutoSize = true;
+            this.dialogLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dialogLabel.Location = new System.Drawing.Point(3, 0);
+            this.dialogLabel.Name = "dialogLabel";
+            this.dialogLabel.Size = new System.Drawing.Size(278, 181);
+            this.dialogLabel.TabIndex = 3;
+            this.dialogLabel.Text = "Enter value:";
+            // 
+            // TextBoxDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "TextBoxDialog";
+            this.Text = "Enter value";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button cancelBtn;
+        private System.Windows.Forms.Button okBtn;
+        private System.Windows.Forms.TextBox valueTextBox;
+        private System.Windows.Forms.Label dialogLabel;
+    }
+}
---
+++ libnetool/Dialogs/TextBoxDialog.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace Netool.Dialogs
+{
+    public partial class TextBoxDialog : Form
+    {
+        /// <summary>
+        /// Gets value entered by user - only valid if DialogResult is OK
+        /// </summary>
+        public string Value { get; private set; }
+
+        /// <summary>
+        /// This event occurs when the TextBox is being validated, sender will be the TextBox not TextBoxDialog
+        /// </summary>
+        public event EventHandler<CancelEventArgs> ValidatingValue;
+
+        public string DialogTitle { get { return this.Text; } set { this.Text = value; } }
+
+        public string DialogText { get { return dialogLabel.Text; } set { dialogLabel.Text = value; } }
+
+        public TextBoxDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void okBtn_Click(object sender, EventArgs e)
+        {
+            Value = valueTextBox.Text;
+        }
+
+        private void valueTextBox_Validating(object sender, CancelEventArgs e)
+        {
+            if (ValidatingValue != null) ValidatingValue(sender, e);
+        }
+    }
+}
