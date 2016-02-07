9e52f6b94d3e1b2af647e5fdd48b3c872e4f3dd3
Hynek Schlindenbuch <schlindenbuch.h@seznam.cz>
2016-02-07 14:56:30 +0100

    add MaskedStream for future WebSocket support

---
+++ Tests/Http/Network/DataFormats/WebSocket/MaskedStreamTests.cs
@@ -0,0 +1,41 @@
+﻿using Netool.Network.DataFormats;
+using Netool.Network.DataFormats.WebSocket;
+using Xunit;
+
+namespace Tests.Http.Network.DataFormats.WebSocket
+{
+    public class MaskedStreamTests
+    {
+        [Fact]
+        public void TestReadByte()
+        {
+            var mask = new byte[] { 0, 1, 128, 255 };
+            var innerStream = new ByteArray(new byte[] { 0, 1, 129, 5, 15, 16, 17, 0 });
+            var stream = new MaskedStream(mask, innerStream);
+            Assert.Equal(innerStream.Length, stream.Length);
+            Assert.Equal(0, stream.ReadByte(0));
+            Assert.Equal(0, stream.ReadByte(1));
+            Assert.Equal(1, stream.ReadByte(2));
+            Assert.Equal(250, stream.ReadByte(3));
+            Assert.Equal(15, stream.ReadByte(4));
+            Assert.Equal(17, stream.ReadByte(5));
+            Assert.Equal(145, stream.ReadByte(6));
+            Assert.Equal(255, stream.ReadByte(7));
+        }
+
+        [Fact]
+        public void TestReadBytesToBuffer()
+        {
+            var mask = new byte[] { 0, 1, 128, 255 };
+            var innerStream = new ByteArray(new byte[] { 0, 1, 129, 5, 15, 16, 17, 0 });
+            var stream = new MaskedStream(mask, innerStream);
+            var buffer = new byte[6];
+            stream.ReadBytesToBuffer(buffer, 1, buffer.Length - 1, 1);
+            Assert.Equal(0, buffer[1]);
+            Assert.Equal(1, buffer[2]);
+            Assert.Equal(250, buffer[3]);
+            Assert.Equal(15, buffer[4]);
+            Assert.Equal(17, buffer[5]);
+        }
+    }
+}
---
+++ http/Network/DataFormats/WebSocket/MaskedStream.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Netool.Network.DataFormats.WebSocket
+{
+    /// <summary>
+    /// Stream that masks it's inner stream by XORing it with given mask
+    /// </summary>
+    /// <remarks>
+    /// Used for https://tools.ietf.org/html/rfc6455#section-5.3
+    /// Apply a MaskedStream with the same key again to unmask it.
+    /// </remarks>
+    public class MaskedStream : IDataStream
+    {
+        /// <inheritdoc/>
+        public long Length { get { return innerStream.Length; } }
+
+        private IDataStream innerStream;
+        private byte[] mask;
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="mask"></param>
+        /// <param name="innerStream"></param>
+        /// <exception cref="ArgumentNullException">mask or innerStream</exception>
+        /// <exception cref="ArgumentException">mask length is not 4</exception>
+        public MaskedStream(byte[] mask, IDataStream innerStream)
+        {
+            if (mask == null) throw new ArgumentNullException("mask");
+            if (innerStream == null) throw new ArgumentNullException("innerStream");
+            if (mask.Length != 4) throw new ArgumentException("mask length is not 4!");
+            this.mask = new byte[4];
+            Array.Copy(mask, this.mask, 4);
+            this.innerStream = innerStream;
+        }
+
+        /// <inheritdoc/>
+        public byte ReadByte(long index)
+        {
+            var b = innerStream.ReadByte(index);
+            return (byte)(b ^ mask[index % 4]);
+        }
+
+        /// <inheritdoc/>
+        public void ReadBytesToBuffer(byte[] buffer, long start = 0, int length = -1, int offset = 0)
+        {
+            IDataStreamHelpers.ReadBytesToBufferArgsCheck(this, buffer, start, ref length, offset);
+            innerStream.ReadBytesToBuffer(buffer, start, length, offset);
+            for (long i = 0; i < length; ++i)
+            {
+                buffer[offset + i] = (byte)(buffer[offset + i] ^ mask[(start + i) % 4]);
+            }
+        }
+
+        /// <inheritdoc/>
+        public object Clone()
+        {
+            var c = (IDataStream)innerStream.Clone();
+            if (object.ReferenceEquals(c, innerStream)) return this;
+            return new MaskedStream(mask, c);
+        }
+    }
+}
