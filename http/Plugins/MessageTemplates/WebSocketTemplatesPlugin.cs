131281b9a9a4dd83199c060ecb4c32dfffb8fcbe
Hynek Schlindenbuch <schlindenbuch.h@seznam.cz>
2016-02-16 18:59:45 +0100

    add WebSocketHandshakeServerTemplate

--- http/Plugins/MessageTemplates/WebSocketTemplatesPlugin.cs
+++ http/Plugins/MessageTemplates/WebSocketTemplatesPlugin.cs
@@ -1,7 +1,10 @@
-﻿using Netool.Network.DataFormats;
+﻿using Netool.Dialogs;
+using Netool.Network.DataFormats;
 using Netool.Network.DataFormats.Http;
 using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Netool.Plugins.MessageTemplates
 {
@@ -33,6 +36,52 @@ namespace Netool.Plugins.MessageTemplates
             }
         }
 
+        public class WebSocketHandshakeServerTemplate : IMessageTemplate
+        {
+            public string Name { get { return "WebSocket/Http/ServerHandshake"; } }
+
+            private static WebSocketHandshakeServerTemplate instance;
+            public static WebSocketHandshakeServerTemplate Instance { get { if (instance == null) instance = new WebSocketHandshakeServerTemplate(); return instance; } }
+            private WebSocketHandshakeServerTemplate() {  }
+
+            /// <inheritdoc/>
+            public IDataStream CreateMessage()
+            {
+                var dialog = new TextBoxDialog("Enter the value of Sec-WebSocket-Key from client request.");
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    return CreateMessage(dialog.Value);
+                }
+                return CreateMessage(null);
+            }
+
+            /// <summary>
+            /// Non-interactive interface for CreateMessage method
+            /// </summary>
+            /// <param name="SecWebsocketKey">null if you don't wish to add Sec-WebSocket-Accept header to the template</param>
+            /// <returns></returns>
+            public IDataStream CreateMessage(string SecWebsocketKey)
+            {
+                var builder = new HttpData.Builder();
+                builder.IsRequest = false;
+                builder.HttpVersion = "1.1";
+                builder.StatusCode = 101;
+                builder.ReasonPhrase = "Switching Protocols";
+                builder.AddHeader("Host", "");
+                builder.AddHeader("Upgrade", "websocket");
+                builder.AddHeader("Connection", "Upgrade");
+                builder.AddHeader("Sec-WebSocket-Version", "13");
+                if(SecWebsocketKey != null)
+                {
+                    var sha1 = new SHA1Managed();
+                    var key = Convert.ToBase64String(sha1.ComputeHash(ASCIIEncoding.ASCII.GetBytes(SecWebsocketKey + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11")));
+                    builder.AddHeader("Sec-WebSocket-Accept", key);
+                }
+                
+                return builder.CreateAndClear();
+            }
+        }
+
         /// <inheritdoc/>
         public string Author { get { return "Hynek Schlindenbuch"; } }
 
@@ -51,7 +100,11 @@ namespace Netool.Plugins.MessageTemplates
         /// <inheritdoc/>
         public IEnumerable<IMessageTemplate> CreateTemplates()
         {
-            return new IMessageTemplate[] { new WebSocketHandshakeClientTemplate() };
+            return new IMessageTemplate[] 
+            {
+                new WebSocketHandshakeClientTemplate(),
+                WebSocketHandshakeServerTemplate.Instance,
+            };
         }
     }
 }
