f1b519c64fa0813bb648f256132aa43837fa37f0
Hynek Schlindenbuch <schlindenbuch.h@seznam.cz>
2016-02-21 17:14:55 +0100

    add scripted proxy driver with websocket support to examples

---
+++ Examples/ChannelDrivers/CSScript/WebSocketDefaultProxyDriver.cs
@@ -0,0 +1,47 @@
+﻿using Netool.ChannelDrivers;
+using Netool.Network;
+using Netool.Network.DataFormats;
+using Netool.Network.DataFormats.Http;
+using Netool.Network.Http;
+using Netool.Plugins.Http.ProtocolUpgrades;
+
+namespace Examples.ChannelDrivers.CSScript
+{
+    public class WebSocketDefaultProxyDriver : DefaultProxyDriver
+    {
+        private static WebSocketPlugin.WebSocketProtocolUpgrader upgrader = WebSocketPlugin.WebSocketProtocolUpgrader.Instance;
+
+        protected override IDataStream responseMapper(IProxyChannel c, IDataStream s)
+        {
+            var channel = c as DefaultProxyChannel;
+            var data = s as HttpData;
+            if (channel != null && data != null && data.Code == 101)
+            {
+                string upgrade;
+                if (data.Headers.TryGetValue("Upgrade", out upgrade) && upgrade.ToLower() == "websocket")
+                {
+                    channel.ReplaceInnerChannels(upgradeServer, upgradeClient);
+                }
+            }
+            return s;
+        }
+
+        private void upgradeServer(IServerChannel c)
+        {
+            var channel = c as HttpServerChannel;
+            if (channel != null)
+            {
+                channel.UpgradeProtocol(upgrader);
+            }
+        }
+
+        private void upgradeClient(IClientChannel c)
+        {
+            var channel = c as HttpClientChannel;
+            if (channel != null)
+            {
+                channel.UpgradeProtocol(upgrader);
+            }
+        }
+    }
+}
